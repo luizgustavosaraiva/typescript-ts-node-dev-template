cfb03559dfa818d22bfc62ad5f788f4872387d64
Luiz Gustavo Saraiva <lg.saraiva@outlook.com>
2021-12-17 07:46:55 -0300

    Adds tips to rename App

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,3 @@
 console.log(
-  '\n\n--- Aplicação Funcionando ---\n--- Lembrar de atualizar as dependências caso preciso ---\n\n'
+  '\n\n--- App running ---\n--- Tip: update dependencies if necessary---\n\n--- Rename YOUR_APP_NAME_HERE in package.json'
 );
